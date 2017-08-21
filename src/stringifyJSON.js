d37573c2de2b0a1c958464174d22abba5a721821
Lyddem <lydiademi@gmail.com>
2017-08-21 14:59:22 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,7 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-    if(obj === '{}'){
-      return;
+  if(typeof obj === 'number' || typeof obj === 'boolean'){
+    return '' + obj;
+  }
+
+  if(!obj){
+    return 'null';
+  }
+
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+
+  if(Array.isArray(obj)){
+    var results = [];
+
+    if(obj.length === 0){
+      return '[]';
     }
-};  s
+    obj.forEach(elem => {
+      results.push(stringifyJSON(elem));
+    })
+      return '[' + results.join(',') + ']';
+  }
+
+  if(typeof obj === 'object'){
+
+    var results = [];
+
+    for(var key in obj){
+      if(typeof obj[key] === 'function' || obj[key] === undefined){ return '{}'}
+
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+      return '{' +  results.join(',') + '}';
+  }
+
+
+};
